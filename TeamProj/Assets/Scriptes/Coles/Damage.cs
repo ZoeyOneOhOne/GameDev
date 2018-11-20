af79a1530ead55346a76f831454f4bf28ac38be5
Cole Perry <perry391@marshall.edu>
2018-11-20 00:45:21 -0500

    Added final boss and animations

---
+++ TeamProj/Assets/Scriptes/Coles/Damage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    public float damageAmount = 25;
+
+    private void Start()
+    {
+     
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        EnemyHealth bh = col.gameObject.GetComponent<EnemyHealth>();
+        Health h = col.gameObject.GetComponent<Health>();
+        if (bh != null)
+            bh.ChangeHealth(-damageAmount);
+        if (h != null)
+        {
+            h.ChangeHealth(-damageAmount);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        EnemyHealth bh = col.gameObject.GetComponent<EnemyHealth>();
+        Health h = col.gameObject.GetComponent<Health>();
+        if(bh != null)
+        bh.ChangeHealth(-damageAmount);
+        if (h != null)
+        {
+            h.ChangeHealth(-damageAmount);
+            Destroy(gameObject);
+        }
+    }
+
+}
