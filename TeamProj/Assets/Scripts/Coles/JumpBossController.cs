ee0f3d0ffb20d13bc0703da4aa2981e4d11e3d60
Cole Perry <perry391@marshall.edu>
2018-11-20 18:18:44 -0500

    added explosion particle system
    Its not done yet tho

---
+++ TeamProj/Assets/Scripts/Coles/JumpBossController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBossController : MonoBehaviour {
+
+    public float speed;
+    float Distance_;
+    Vector3 playerPos;
+    Vector3 newXPos;
+    private Rigidbody2D rb;
+    private float nextActionTime = 0.0f;
+    public float period = .5f;
+    public Animator animator;
+
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        GameObject player = God.playerObject;
+        playerPos = player.transform.position;
+        newXPos = new Vector3(playerPos.x, playerPos.y, 0);
+
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            rb.velocity = new Vector2(rb.velocity.x, 10);
+        }
+
+        if (player)
+        {
+            //Following Player
+            Distance_ = Vector3.Distance(gameObject.transform.position, player.transform.position);
+            if (Distance_ <= 10f)
+            {
+                animator.SetFloat("distance", Mathf.Abs(Distance_));
+                transform.position = Vector3.MoveTowards(transform.position, newXPos, speed);
+            }
+        }
+
+    }
+}
---
+++ TeamProj/Assets/Scripts/Coles/SpriteSpin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSpin : MonoBehaviour {
+
+    float startXScale;
+    float currentXScale;
+
+    // Use this for initialization
+    void Start () {
+
+        startXScale = transform.localScale.x;
+        currentXScale = startXScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentXScale += startXScale + Mathf.Sin(Time.time);
+        transform.localScale = new Vector3(currentXScale, transform.localScale.y, transform.localScale.z);
+		
+	}
+}
