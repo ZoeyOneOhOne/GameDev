0c7095cb8530f1f69909bd904262269d0dba4fa8
Cole Perry <perry391@marshall.edu>
2018-11-28 11:01:31 -0500

    Added Jack's AI

--- TeamProj/Assets/Scripts/Coles/EnemyController.cs
+++ TeamProj/Assets/Scripts/Coles/EnemyController.cs
@@ -17,11 +17,6 @@ public class EnemyController : MonoBehaviour {
         playerPos = player.transform.position;
         newXPos = new Vector3(playerPos.x, 0, 0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += Vector3.up * speed * Time.deltaTime;
-        }
-
         if (player) {
 
             Distance_ = Vector3.Distance(gameObject.transform.position, player.transform.position);
