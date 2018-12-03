6122e3f0617980f94fb0f08f0ed2105b91754a1c
Dylan White <44742010+Obscurum12@users.noreply.github.com>
2018-12-02 22:22:45 -0500

    made moving platform script

---
+++ TeamProj/Assets/Scripts/Dylans/PlotformMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlotformMovement : MonoBehaviour {
+
+    public Slider slider;
+    public Vector3 startPoint, endPoint;
+    float currentPercentage = 0;
+    public float travelSpeed = 1;
+
+    void Start()
+    {
+        startPoint = transform.GetChild(0).position;
+        endPoint = transform.GetChild(1).position;
+        slider = (Slider)gameObject.GetComponentInChildren(typeof(Slider));
+    }
+
+    void Update()
+    {
+        currentPercentage = Mathf.PingPong(Time.time, travelSpeed);
+        if (slider)
+            slider.value = currentPercentage;
+        transform.position = Vector3.Lerp(startPoint, endPoint, currentPercentage);
+    }
+
+}
