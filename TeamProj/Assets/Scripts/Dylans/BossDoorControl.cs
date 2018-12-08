8553bd2149c400cf850b16f31f465c13833b6e02
Dylan White <44742010+Obscurum12@users.noreply.github.com>
2018-12-08 00:23:17 -0500

    Added Level 2 stuff
    Locked boss door wtih a control panel

---
+++ TeamProj/Assets/Scripts/Dylans/BossDoorControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BossDoorControl : MonoBehaviour {
+
+
+    public Transform redLight;
+    bool bossDoorOpen = false; 
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject != God.playerObject || bossDoorOpen == true)
+        {
+            return;
+        }
+        else
+        {
+            if (redLight)
+                Instantiate(redLight, transform.GetChild(0).transform.position, transform.rotation);
+
+            if(bossDoorOpen == false)
+            {
+                Destroy(GameObject.FindWithTag("Boss Door"));
+            }
+        }
+    }
+}
